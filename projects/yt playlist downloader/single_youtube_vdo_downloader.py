1447bf2ad19a5c8c8c2f9e58c3139384118c32cc
jonash-chataut <jonaschataut777@gmail.com>
2025-09-18 20:38:43 +0545

    Added the single youtube video downloader using py

---
+++ projects/yt playlist downloader/single_youtube_vdo_downloader.py
@@ -0,0 +1,16 @@
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+url = input("Enter the url of the video: ")
+
+yt = YouTube(url, on_progress_callback=on_progress)
+print("Title: ", yt.title)
+
+#here the path is in the same folder in which the code is so i gave directly if you want you can give the custom path of your own choice
+download_path = "downloads" 
+
+ys = yt.streams.get_highest_resolution()
+ys.download(output_path=download_path)
+
+print("Download completed!")
+
