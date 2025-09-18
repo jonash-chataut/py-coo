b2c5eb7323e6419d58ad63dd78e2fe4a2407848a
jonash-chataut <jonaschataut777@gmail.com>
2025-09-18 21:12:07 +0545

    replaced the pytube with pytubefix

--- projects/yt_video_downloader_py/yt_playlist_downloader.py
+++ projects/yt_video_downloader_py/yt_playlist_downloader.py
@@ -1,6 +1,6 @@
-# Playlist downloader with Pytube
-from pytube import Playlist, YouTube
 import os
+from pytubefix import Playlist, YouTube
+from pytubefix.cli import on_progress_bar  # for progress bar
 
 # Ask for playlist link
 playlist_link = input("Enter the playlist link: ")
@@ -9,20 +9,20 @@ playlist_link = input("Enter the playlist link: ")
 p = Playlist(playlist_link)
 
 # Make downloads folder
-download_path = "downloads"
+download_path = "playlist_downloads"
 os.makedirs(download_path, exist_ok=True)
 
 print(f"\nDownloading playlist: {p.title}")
-print(f"Total videos: {len(p.videos)}\n")
+print(f"Total videos: {len(p.video_urls)}\n")
 
-# Download videos
-for video in p.videos:
+# Download videos one by one
+for url in p.video_urls:
     try:
-        yt = YouTube(video.watch_url)
-        print(f"Downloading: {yt.title}...")
+        yt = YouTube(url, on_progress_callback=on_progress_bar)
+        print(f"\nDownloading: {yt.title}...")
         yt.streams.get_highest_resolution().download(output_path=download_path)
-        print(f"Download completed! {video.title} \n")
+        print(f"Download completed: {yt.title}\n")
     except Exception as e:
-        print(f"Failed to download {video.title}: {e}\n")
+        print(f"Failed to download {url}: {e}\n")
 
 print("All videos processed!")
