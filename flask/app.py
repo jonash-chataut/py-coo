90ddf68cf4c920100198318d1eb876ed32f7ad56
jonash-chataut <jonaschataut777@gmail.com>
2025-09-26 21:01:56 +0545

    flask leaning

---
+++ flask/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todolist.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(200), nullable = False)
+    desc = db.Column(db.String(500), nullable = False)
+    date_executed = db.Column(db.DateTime , default = datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.title}"
+
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    if request.method == "POST":
+        title=request.form['title']
+        desc= request.form['desc']
+        todo = Todo(title=title,desc = desc)
+        db.session.add(todo)
+        db.session.commit()
+    alltodos = Todo.query.all()
+    return render_template("index.html", alltodos=alltodos)
+
+@app.route("/products")
+def products():
+    return "The products are here"
+
+if __name__ == "__main__":
+    app.run(debug=True,port=8000)
