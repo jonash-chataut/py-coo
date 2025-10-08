57344c38311c4217604b91f99aa84d02372dd4b5
jonash-chataut <jonaschataut777@gmail.com>
2025-10-08 12:56:27 +0545

    NUmpy add

---
+++ learnings/libraries learning/numpy/code learnings/basics.py
@@ -0,0 +1,23 @@
+import numpy as np
+list = [1,2,6,5,4]
+# numpy array make -> np.array()
+arr = np.array(list)
+print(arr)
+
+# filled_by_zeros_arr = np.zeros((2,3))
+# print(filled_by_zeros_arr)
+
+# filled_by_specific_arr = np.full((2,3),7)
+# print(filled_by_specific_arr)
+
+# # identity matrix creating
+# id_mat=np.eye(4)
+# print(id_mat)
+
+
+# print(arr%2)
+
+# np.delete(arr,,axis=None)
+
+print(np.split(arr,2))
+
